a2f49813cf42c572c7fb74c53803aee4feefa8cb
Murat GÜVENÇ <muratguvenc.py@gmail.com>
2022-06-05 14:37:17 +0300

    data columns has been edited

--- HouseRentTest/houseRent.py
+++ HouseRentTest/houseRent.py
@@ -4,3 +4,7 @@ data = pd.read_csv('houses_to_rent.csv')
 
 data = data [['city', 'rooms', 'bathroom', 'parking spaces', 'fire insurance', 'furniture', 'rent amount']]
 print(data.head())
+
+data['rent amount'] = data['rent amount'].map(lambda i: int(i[2:].replace(',', '')))
+data['fire insurance'] = data['fire insurance'].map(lambda i: int(i[2:]))
+print(data.head())
