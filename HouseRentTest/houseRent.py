16629fb11016fc338af4cf7427edf43045e34ae1
Murat GÜVENÇ <muratguvenc.py@gmail.com>
2022-06-05 14:49:48 +0300

    data columns has been edited

--- HouseRentTest/houseRent.py
+++ HouseRentTest/houseRent.py
@@ -1,5 +1,5 @@
 import pandas as pd
-
+from sklearn import preprocessing
 data = pd.read_csv('houses_to_rent.csv')
 
 data = data [['city', 'rooms', 'bathroom', 'parking spaces', 'fire insurance', 'furniture', 'rent amount']]
@@ -7,4 +7,7 @@ print(data.head())
 
 data['rent amount'] = data['rent amount'].map(lambda i: int(i[2:].replace(',', '')))
 data['fire insurance'] = data['fire insurance'].map(lambda i: int(i[2:]))
+
+le = preprocessing.LabelEncoder()
+data['furniture'] = le.fit_transform(data['furniture'])
 print(data.head())
