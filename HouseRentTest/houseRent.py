632606dbb1022af1203f399dddce0d5ae10f9052
Murat GÜVENÇ <muratguvenc.py@gmail.com>
2022-06-05 16:12:00 +0300

    split data

--- HouseRentTest/houseRent.py
+++ HouseRentTest/houseRent.py
@@ -1,13 +1,30 @@
 import pandas as pd
+import numpy as np
+from numpy import ndarray
 from sklearn import preprocessing
-data = pd.read_csv('houses_to_rent.csv')
 
-data = data [['city', 'rooms', 'bathroom', 'parking spaces', 'fire insurance', 'furniture', 'rent amount']]
+# IMPORT DATA
+data = pd.read_csv('houses_to_rent.csv')
+data = data[['city', 'rooms', 'bathroom', 'parking spaces', 'fire insurance', 'furniture', 'rent amount']]
+
 print(data.head())
 
+# MAP DATA
 data['rent amount'] = data['rent amount'].map(lambda i: int(i[2:].replace(',', '')))
 data['fire insurance'] = data['fire insurance'].map(lambda i: int(i[2:]))
 
 le = preprocessing.LabelEncoder()
 data['furniture'] = le.fit_transform(data['furniture'])
+
 print(data.head())
+
+# CHECKING NULL DATA
+print(data.isnull().sum())
+# data = data.dropna() If data is null
+
+# SPLIT DATA#
+x = np.array(data.drop(['rent amount'], 1))
+y = np.array(data['rent amount'])
+
+print('X', x.shape)
+print('Y', y.shape)
